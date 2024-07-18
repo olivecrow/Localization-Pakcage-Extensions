a100217978ad60def89600d74a0378359c335c03
olivecrow-Main <olivecrow.games@gmail.com>
2024-07-18 14:40:50 +0900

    Add ApplyLocalizedString overrides for legacy text components

--- Runtime/Scripts/LocalizationUtility.cs
+++ Runtime/Scripts/LocalizationUtility.cs
@@ -1,6 +1,8 @@
 ﻿using TMPro;
+using UnityEngine;
 using UnityEngine.Localization.Settings;
 using UnityEngine.Localization.Tables;
+using UnityEngine.UI;
 
 namespace LocalizationPackageExtensions
 {
@@ -8,6 +10,7 @@ namespace LocalizationPackageExtensions
     {
         /// <summary>
         /// Find a localized string from a table and apply it directly to a TMP component.
+        /// It can be used for both UI and 3D TextMesh.
         /// This method operates asynchronously.
         /// </summary>
         public static void ApplyLocalizedString(TableReference tableReference, TableEntryReference entryReference, TMP_Text tmp)
@@ -15,6 +18,26 @@ namespace LocalizationPackageExtensions
             tmp.text = null;
             var async = LocalizationSettings.StringDatabase.GetLocalizedStringAsync(tableReference, entryReference);
             async.Completed += handle => tmp.text = handle.Result;
+        }
+        /// <summary>
+        /// Find a localized string from a table and apply it directly to a Legacy Text component.
+        /// This method operates asynchronously.
+        /// </summary>
+        public static void ApplyLocalizedString(TableReference tableReference, TableEntryReference entryReference, Text text)
+        {
+            text.text = null;
+            var async = LocalizationSettings.StringDatabase.GetLocalizedStringAsync(tableReference, entryReference);
+            async.Completed += handle => text.text = handle.Result;
+        }
+        /// <summary>
+        /// Find a localized string from a table and apply it directly to a Legacy TextMesh component.
+        /// This method operates asynchronously.
+        /// </summary>
+        public static void ApplyLocalizedString(TableReference tableReference, TableEntryReference entryReference, TextMesh textMesh)
+        {
+            textMesh.text = null;
+            var async = LocalizationSettings.StringDatabase.GetLocalizedStringAsync(tableReference, entryReference);
+            async.Completed += handle => textMesh.text = handle.Result;
         }
     }
 }
